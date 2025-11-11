2e63f0bd7509208ce58eecfd1d1c0fc6284133aa
ichaoui56 <ilyaschaoui2002@gmail.com>
2025-11-11 03:14:44 +0100

    create spring boot project

---
+++ src/test/java/com/progresssoft/fxDealsWarehouse/FxDealsWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progresssoft.fxDealsWarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxDealsWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
